ba0c40d82ba0b703bc3b6b48adb78dd419d88b18
francis.ferdinard <francis.ferdinard@PROGRAM1>
2024-09-08 16:20:10 -0500

    Chapter 12 (Changes)

--- FutureValue/Controllers/HomeController.cs
+++ FutureValue/Controllers/HomeController.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FutureValue.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FutureValue.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.Name = "Mary";
             ViewBag.FV = 99999.99;
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
         }
     }
 }
